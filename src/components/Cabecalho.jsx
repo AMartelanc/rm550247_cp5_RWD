2c4de0a318f60a51ea2b1e69b16541b716371c37
AMartelanc <alemartelanc@gmail.com>
2023-09-05 00:59:57 -0300

    cabeçalho, conteudo e rodapé

---
+++ src/components/Cabecalho.jsx
@@ -0,0 +1,16 @@
+export default function Cabecalho() {
+    return (
+        <>
+        <header>
+            <h1>
+                <ul>
+                    <li><a href="#">Item - 1</a></li>
+                    <li><a href="#">Item - 2</a></li>
+                    <li><a href="#">Item - 3</a></li>
+                </ul>
+            </h1>
+        </header>
+        </>
+    );
+
+}
---
+++ src/components/Rodape.jsx
@@ -0,0 +1,16 @@
+export default function Rodape() {
+    return (
+        <>
+        <footer>
+            <div>
+                <ul>
+                    <li><a href="#">Github</a></li>
+                    <li><a href="#">Facebook</a></li>
+                    <li><a href="#">Twitter</a></li>
+                </ul>
+            </div>
+        </footer>
+        </>
+    );
+
+}
