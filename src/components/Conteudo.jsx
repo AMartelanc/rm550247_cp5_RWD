f414a7d0d2d792da2a0bc2ee6b2bbe3c350924a9
AMartelanc <alemartelanc@gmail.com>
2023-09-05 00:53:41 -0300

    cabeçalho, conteudo e rodapé

---
+++ src/components/Conteudo.jsx
@@ -0,0 +1,13 @@
+export default function Rodape() {
+    return (
+        <>
+        <section>
+            <div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque volutpat massa eget velit semper, nec luctus massa mollis. Nulla facilisis mauris eu tristique interdum. Nullam malesuada lacinia arcu, ac ultricies justo rutrum posuere. Donec sapien lectus, lacinia nec purus ac, mattis tincidunt ante. Mauris interdum elit massa, placerat consequat eros.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc odio lectus, pharetra vel malesuada a, tempor ut mauris. Sed lobortis dolor non massa auctor dignissim eu vitae purus. Quisque nec ultrices velit. Duis a sem nec tortor consectetur tincidunt. Cras sit amet elit vitae mi vulputate cursus. In feugiat a.</p>
+            </div>
+        </section>
+        </>
+    );
+
+}
